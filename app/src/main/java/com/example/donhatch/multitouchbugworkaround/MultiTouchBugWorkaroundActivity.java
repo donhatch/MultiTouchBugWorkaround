faeb229e8edaa771d5a0e2ee8e53a917e8674f45
Don Hatch <hatch@plunk.org>
2018-04-25 21:03:58 -0700

    initial files.  probably I forgot some.

---
+++ app/src/main/java/com/example/donhatch/multitouchbugworkaround/MultiTouchBugWorkaroundActivity.java
@@ -0,0 +1,12 @@
+package com.example.donhatch.multitouchbugworkaround;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultiTouchBugWorkaroundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
